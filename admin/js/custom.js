90f152c612cd0fcd0bd906e97f009de2366635d4
mehmet-karagoz <atleownhem@gmail.com>
2022-04-09 14:40:17 +0300

    added admin car page

--- admin/js/custom.js
+++ admin/js/custom.js
@@ -1,4 +1,17 @@
 function deleteRow(r) {
-    var i = r.parentNode.parentNode.rowIndex;
-    document.getElementById("myTable").deleteRow(i);
+  var i = r.parentNode.parentNode.rowIndex;
+  document.getElementById("myTable").deleteRow(i);
+}
+
+
+function showAlert(params) {
+  var successAlert = document.getElementById('success');
+  successAlert.style.display = '';
+}
+
+function showAlertDanger(params) {
+
+  if (confirm('Do you want to remove the car?')) {
+    location.href = "car.html";
   }
+}
