984f925fa95ee3b6aaa25348bd9ab5c9f75c6eea
mehmet-karagoz <atleownhem@gmail.com>
2022-05-27 19:03:01 +0300

    customer message

--- customer/contact.php
+++ customer/contact.php
@@ -4,242 +4,208 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Car Rental Software</title>
 
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-      crossorigin="anonymous"
-    />
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Car Rental Software</title>
 
-    <link rel="stylesheet" href="css/style.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
 
-    <script
-      src="https://kit.fontawesome.com/8df3f43431.js"
-      crossorigin="anonymous"
-    ></script>
-  </head>
+  <link rel="stylesheet" href="css/style.css" />
 
-  <body>
-    <!--Header-->
-    <header>
-      <nav class="navbar navbar-expand-lg">
-        <div class="container">
-          <a class="navbar-brand" href="index.php">
-            <h2>Car Rental <em>Software</em></h2>
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="index.php"
-                  >Home</a
-                >
-              </li>
-           
-              <li class="nav-item">
-                <a class="nav-link" href="about-us.php">About Us</a>
-              </li>
+  <script src="https://kit.fontawesome.com/8df3f43431.js" crossorigin="anonymous"></script>
+</head>
 
-              <li class="nav-item active">
-                <a class="nav-link" href="contact.php">Contact Us</a>
-              </li>
-              <?php
-              if (!isset($_SESSION["customerId"])) {
-                echo '<li class="nav-item">
+<body>
+  <!--Header-->
+  <header>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">
+          <h2>Car Rental <em>Software</em></h2>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="index.php">Home</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="about-us.php">About Us</a>
+            </li>
+
+            <li class="nav-item active">
+              <a class="nav-link" href="contact.php">Contact Us</a>
+            </li>
+            <?php
+            if (!isset($_SESSION["customerId"])) {
+              echo '<li class="nav-item">
                 <a class="nav-link" href="login.php">Login</a>
               </li>';
-              }else {
-                echo '<li class="nav-item">
+            } else {
+              echo '<li class="nav-item">
                 <a class="nav-link" href="profile.php">My Profile</a>
               </li>';
-              }
-              ?>
+            }
+            ?>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <!-- Page Content -->
+  <div class="page-heading contact-heading header-text" style="background-image: url(../images/heading-4-1920x500.jpg)">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="text-content">
+            <h4>Lorem ipsum dolor</h4>
+            <h2>Contact Us</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="find-us">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-heading">
+            <h2>Our Location on Maps</h2>
+          </div>
+        </div>
+        <div class="col-md-8">
+          <div id="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3190.81532691985!2d30.655199124303966!3d36.89476593003152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14c391cd6e38486d%3A0xfbd4e14587332eeb!2sAkdeniz%20%C3%9Cniversitesi!5e0!3m2!1str!2str!4v1649419275366!5m2!1str!2str" width="100%" height="330px" frameborder="0" style="border: 0" allowfullscreen></iframe>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="left-content">
+            <h4>About our office</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisic elit. Sed
+              voluptate nihil eumester consectetur similiqu consectetur.<br /><br />Lorem
+              ipsum dolor sit amet, consectetur adipisic elit. Et,
+              consequuntur, modi mollitia corporis ipsa voluptate corrupti.
+            </p>
+            <ul class="social-icons">
+              <li>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+              </li>
             </ul>
           </div>
         </div>
-      </nav>
-    </header>
-
-    <!-- Page Content -->
-    <div
-      class="page-heading contact-heading header-text"
-      style="background-image: url(../images/heading-4-1920x500.jpg)"
-    >
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="text-content">
-              <h4>Lorem ipsum dolor</h4>
-              <h2>Contact Us</h2>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
+  </div>
 
-    <div class="find-us">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="section-heading">
-              <h2>Our Location on Maps</h2>
-            </div>
-          </div>
-          <div class="col-md-8">
-            <div id="map">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3190.81532691985!2d30.655199124303966!3d36.89476593003152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14c391cd6e38486d%3A0xfbd4e14587332eeb!2sAkdeniz%20%C3%9Cniversitesi!5e0!3m2!1str!2str!4v1649419275366!5m2!1str!2str"
-                width="100%"
-                height="330px"
-                frameborder="0"
-                style="border: 0"
-                allowfullscreen
-              ></iframe>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="left-content">
-              <h4>About our office</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisic elit. Sed
-                voluptate nihil eumester consectetur similiqu consectetur.<br /><br />Lorem
-                ipsum dolor sit amet, consectetur adipisic elit. Et,
-                consequuntur, modi mollitia corporis ipsa voluptate corrupti.
-              </p>
-              <ul class="social-icons">
-                <li>
-                  <a href="#"><i class="fa fa-facebook"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="fa fa-twitter"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="fa fa-linkedin"></i></a>
-                </li>
-              </ul>
-            </div>
+  <div class="send-message">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-heading">
+            <h2>Send us a Message</h2>
           </div>
         </div>
-      </div>
-    </div>
-
-    <div class="send-message">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="section-heading">
-              <h2>Send us a Message</h2>
-            </div>
-          </div>
-          <div class="col-md-8">
-            <div class="contact-form">
-              <form id="contact" action="" method="get">
-                <div class="row">
-                  <div class="col-lg-12 col-md-12 col-sm-12">
-                    <fieldset>
-                      <input
-                        name="name"
-                        type="text"
-                        class="form-control"
-                        id="name"
-                        placeholder="Full Name"
-                        required=""
-                      />
-                    </fieldset>
-                  </div>
-                  <div class="col-lg-12 col-md-12 col-sm-12">
-                    <fieldset>
-                      <input
-                        name="email"
-                        type="text"
-                        class="form-control"
-                        id="email"
-                        placeholder="E-Mail Address"
-                        required=""
-                      />
-                    </fieldset>
-                  </div>
-                  <div class="col-lg-12 col-md-12 col-sm-12">
-                    <fieldset>
-                      <input
-                        name="subject"
-                        type="text"
-                        class="form-control"
-                        id="subject"
-                        placeholder="Subject"
-                        required=""
-                      />
-                    </fieldset>
-                  </div>
-                  <div class="col-lg-12">
-                    <fieldset>
-                      <textarea
-                        name="message"
-                        rows="6"
-                        class="form-control"
-                        id="message"
-                        placeholder="Your Message"
-                        required=""
-                      ></textarea>
-                    </fieldset>
-                  </div>
-                  <div class="col-lg-12">
-                    <fieldset>
-                      <button
-                        type="submit"
-                        id="form-submit"
-                        class="filled-button"
-                      >
-                        Send Message
-                      </button>
-                    </fieldset>
-                  </div>
+        <div class="col-md-8">
+          <div class="contact-form">
+            <form id="contact" action="contact.php" method="post">
+              <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                  <fieldset>
+                    <input name="name" type="text" class="form-control" id="name" placeholder="Full Name" required="" />
+                  </fieldset>
                 </div>
-              </form>
-            </div>
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                  <fieldset>
+                    <input name="email" type="text" class="form-control" id="email" placeholder="E-Mail Address" required="" />
+                  </fieldset>
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                  <fieldset>
+                    <input name="subject" type="text" class="form-control" id="subject" placeholder="Subject" required="" />
+                  </fieldset>
+                </div>
+                <div class="col-lg-12">
+                  <fieldset>
+                    <textarea name="message" rows="6" class="form-control" id="message" placeholder="Your Message" required=""></textarea>
+                  </fieldset>
+                </div>
+                <div class="col-lg-12">
+                  <fieldset>
+                    <button type="submit" id="form-submit" name="sendMessage" class="filled-button">
+                      Send Message
+                    </button>
+                  </fieldset>
+                </div>
+              </div>
+            </form>
           </div>
-          <div class="col-md-4">
-            <img src="../images/team_01.jpg" class="img-fluid mt-3" alt="" />
+        </div>
+        <div class="col-md-4">
+          <img src="../images/team_01.jpg" class="img-fluid mt-3" alt="" />
 
-            <h5 class="text-center" style="margin-top: 15px">John Doe</h5>
+          <h5 class="text-center" style="margin-top: 15px">John Doe</h5>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php
+  require_once "../config.php";
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+    $customerId = "";
+    if (isset($_SESSION["customerId"])) {
+      $customerId = $_SESSION["customerId"];
+      $contactSql = "INSERT INTO message (name,email,subject,message,customer_id) VALUES ('$name','$email', '$subject','$message'," . $customerId . ")";
+      if (mysqli_query($link, $contactSql)) {
+
+        echo '<script>alert("Successfully sent");window.location.href="index.php";</script>';
+      }
+    } else {
+      $contactSql = "INSERT INTO message (name,email,subject,message) VALUES ('$name','$email', '$subject','$message')";
+
+      if (mysqli_query($link, $contactSql)) {
+
+        echo '<script>alert("Successfully sent");window.location.href="index.php";</script>';
+        
+      }
+    }
+  }
+  ?>
+
+  <!-- Footer -->
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="inner-content">
+            <p>Copyright © 2022 Mehmet Karagoz</p>
           </div>
         </div>
       </div>
     </div>
+  </footer>
 
-    <!-- Footer -->
-    <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="inner-content">
-              <p>Copyright © 2022 Mehmet Karagoz</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
 
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-      crossorigin="anonymous"
-    ></script>
-  </body>
 </html>
